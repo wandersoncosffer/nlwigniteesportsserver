5e27daef77b63c487b9db647ba911467574dcfda
Wanderson Cosffer <wandersoncosffer@gmail.com>
2022-09-12 10:03:33 -0300

    new: Configured node enviroment

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/ads', (req, res) => {
+  return res.status(200).json([
+    {id: 1, name: 'Anúncio 1' },
+    {id: 2, name: 'Anúncio 2' },
+    {id: 3, name: 'Anúncio 3'  },
+    {id: 4, name: 'Anúncio 4' }
+  ])
+});
+
+app.listen('3333', () => {
+  console.log('Backend is running!');
+});
